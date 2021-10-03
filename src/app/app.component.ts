ddc527e3b4dcac264e84087da233e02390a57b52
IreneKatrych <irenecherokee@gmail.com>
2021-10-04 02:12:31 +0300

    form done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'form-course-project';
+
+  minDate: Date;
+  maxDate: Date;
+
+  exampleForm: FormGroup;
+  constructor() {
+    this.exampleForm = new FormGroup({
+      userEmail: new FormControl('', [Validators.required, Validators.email]),
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[A-Z]{1}[a-z]{1,20}'),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[A-Z]{1}[a-z]{1,20}'),
+      ]),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]{10}'),
+      ]),
+      dateOfBirth: new FormControl(new Date(), Validators.required),
+      wish: new FormControl(),
+    });
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 100, 0, 1);
+    this.maxDate = new Date();
+  }
+
+  submit(): void {
+    console.log(this.exampleForm);
+  }
 }
